3de13b29e9a0b2714b6c6937c03492615614ab80
prince3103 <prince.sethi@trantorinc.com>
2021-03-24 21:28:49 +0530

    database_server_commit

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
-"""
-This module imports app instance from app module and runs it.
-"""
-from app import app
+from flask import Flask, request
+from skill_database import SkillDatabase
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/data', methods=['GET'])
+def hello():
+    job_role = request.args.get('job_role')
+    skill_type = request.args.get('skill_type')
+    # job_result = SkillDatabase.get_data(job_role=job_role, skill_type='both skill')
+    job_result = SkillDatabase.get_data(job_role=job_role, skill_type=skill_type)
+    if job_result == []:
+        return {'skills': 'No skill found for given input.'}, 404
+    return {'skills': job_result}, 200
+
+if __name__ == '__main__':
+    app.run(port = 8080)
